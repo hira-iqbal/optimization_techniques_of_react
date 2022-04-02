4f41a3c280b367a5ec5c7806263589089a0a4f58
Hira Iqbal <hirach502@gmail.com>
2022-04-02 19:29:05 +0500

    components updates in action

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
-
+import Button from './components/UI/Button/Button';
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+  console.log("App is running");
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph(prevshowParagraph => !prevshowParagraph);
+  }
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      { showParagraph && <p>This is new!</p> }
+      <Button onClick={ toggleParagraphHandler }>ToogleParagraph!</Button>
     </div>
   );
 }
